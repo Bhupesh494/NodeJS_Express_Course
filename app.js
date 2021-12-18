44f8795d769e664896160dda4573919214a01543
Bhupesh Rajpurohit <bhupeshpurohit494@gmail.com>
2021-12-18 13:15:51 +0530

    2.47 minute pert of ideo has been done.

--- app.js
+++ app.js
@@ -9,10 +9,26 @@ stream.on("data", (result) => {
   console.log(result);
 });
 */
-
+/* 
 const _ = require("lodash");
 
 const items = [1, [2, [3, [4]]]];
 
 const newItems = _.flattenDeep(items);
 console.log(newItems);
+console.log("hello bhupesh");
+*/
+
+const { readFile } = require("fs");
+// CHECK FILE PATH
+console.log("start");
+readFile("./content/first.txt", "utf-8", (err, result) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log(result);
+  console.log("done with this task");
+});
+
+console.log("starting next task");
