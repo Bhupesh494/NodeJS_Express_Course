3a5d64f5ab817ed96486aee94da072caf8a209c2
Bhupesh Rajpurohit <bhupeshpurohit494@gmail.com>
2021-12-20 10:45:48 +0530

    Done with async patterns in NodeJS

--- app.js
+++ app.js
@@ -1,34 +1,20 @@
-/*
-const { createReadStream } = require("fs");
-
-const stream = createReadStream("./content/big.txt", "utf8", {
-  highWaterMark: 90000,
-});
-
-stream.on("data", (result) => {
-  console.log(result);
-});
-*/
-/* 
-const _ = require("lodash");
-
-const items = [1, [2, [3, [4]]]];
-
-const newItems = _.flattenDeep(items);
-console.log(newItems);
-console.log("hello bhupesh");
-*/
-
 const { readFile } = require("fs");
-// CHECK FILE PATH
-console.log("start");
-readFile("./content/first.txt", "utf-8", (err, result) => {
-  if (err) {
-    console.log(err);
-    return;
-  }
-  console.log(result);
-  console.log("done with this task");
-});
 
-console.log("starting next task");
+const getText = (path) => {
+  return new Promise((resolve, reject) => {
+    readFile(path, "utf-8", (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    });
+  });
+};
+
+getText("./content/first.txt")
+  .then((result) => console.log(result))
+  .catch((err) => console.log(err));
+getText("./content/second.txt")
+  .then((result) => console.log(result))
+  .catch((err) => console.log(err));
